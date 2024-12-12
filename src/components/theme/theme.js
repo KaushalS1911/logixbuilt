4589801d6d90f95cd8c1c1cb2a61ab49ed934571
Dax-savani <dakshsavani15@gmail.com>
2024-12-12 09:29:35 +0530

    first commit

---
+++ src/components/theme/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {},
+  palette: {
+    black: "#000",
+    white: "#FFFF",
+    blue: '#008DFF',
+    textGray : '#8B9094',
+    gradient : 'linear-gradient(100.7deg, rgba(30, 30, 30, .21) 1.06%, #000E1A 101.06%',
+    skyBlue : '#BFE2FF'
+  },
+});
+export default theme;
