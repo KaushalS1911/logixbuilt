c82c8cc116e0c033e2aff7d6f7ce44fa4134858a
yash49766 <yashkapadiya191@gmail.com>
2024-12-23 09:49:29 +0530

    commit

--- src/components/about/Aboutcompany.jsx
+++ src/components/about/Aboutcompany.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Box, Container, Grid, Typography} from "@mui/material";
-// import bgImg from '../../assets/images/about/Aboutcompany/about-1.png'
-// import bgImg2 from '../../assets/images/about/Aboutcompany/about-2.jpg'
+import bgImg from '../../assets/images/about/Aboutcompany/about-1.png'
+import bgImg2 from '../../assets/images/about/Aboutcompany/about-2.jpg'
 import SettingsIcon from '@mui/icons-material/Settings';
-// import bgImg3 from '../../assets/images/about/Aboutcompany/about-3.webp'
+import bgImg3 from '../../assets/images/about/Aboutcompany/about-3.webp'
 import Image from "next/image";
 import {underline} from "next/dist/lib/picocolors";
 
@@ -13,13 +13,13 @@ function Aboutcompany(props) {
             <Box>
                 <Box
                     sx={{
-                        // background: `url(${bgImg2.src})`,
+                        background: `url(${bgImg2.src})`,
                         height: "405px",
                         width : "100%"
                     }}
                 >
                     <Box sx={{
-                        // background: `url(${bgImg.src})`,
+                        background: `url(${bgImg.src})`,
                         height: "405px",
                         width : "100%",
                         position : "relative"
@@ -102,15 +102,15 @@ function Aboutcompany(props) {
                             overflow: "hidden",
                         }}
                     >
-                        {/*<Image*/}
-                        {/*    style={{*/}
-                        {/*        maxHeight: "100%",*/}
-                        {/*        width: "100%",*/}
-                        {/*        // objectFit: "cover",*/}
-                        {/*    }}*/}
-                        {/*    src={bgImg3}*/}
-                        {/*    alt="img"*/}
-                        {/*/>*/}
+                        <Image
+                            style={{
+                                maxHeight: "100%",
+                                width: "100%",
+                                // objectFit: "cover",
+                            }}
+                            src={bgImg3}
+                            alt="img"
+                        />
                     </Box>
                 </Box>
             </Container>
